aba2ddc21d2813b573c35f13f54fd2a584d12d19
SergeiWal <korovaabc@gmail.com>
2021-04-30 16:23:12 +0300

    Add home page for client, and create her viewModel

--- VMusic/VMusic/ViewModels/Client/ClientMainViewModel.cs
+++ VMusic/VMusic/ViewModels/Client/ClientMainViewModel.cs
@@ -4,13 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using VMusic.Commands;
+using VMusic.Views.Client;
 
 namespace VMusic.ViewModels.Client
 {
     class ClientMainViewModel: BaseWindowViewModel
     {
+        private HomePage homePage;
+        private Page currentPage;
+
         public ClientMainViewModel(Window owner) : base(owner)
         {
+            homePage = new HomePage();
+            homePage.DataContext = new HomePageViewModel();
+
+            CurrentPage = homePage;
+        }
+
+        public Page CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                currentPage = value;
+                OnPropertyChanged("CurrentPage");
+            }
+        }
+
+
+        private Command switchToHomePage;
+
+        public Command SwitchToHomePage
+        {
+            get
+            {
+                return switchToHomePage??(switchToHomePage = new Command((obj) =>
+                {
+                    CurrentPage = homePage;
+                }));
+            }
         }
     }
 }
---
+++ VMusic/VMusic/ViewModels/Client/HomePageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMusic.Repository;
+
+namespace VMusic.ViewModels.Client
+{
+    class HomePageViewModel: BaseViewModel
+    {
+
+        private SongRepository songRepository;
+        public ObservableCollection<SongViewModel> LocalSongList { get; set; }
+
+        public HomePageViewModel()
+        {
+            songRepository = new SongRepository();
+            LocalSongList =
+                new ObservableCollection<SongViewModel>(songRepository.GetAllObject()
+                    .Select(o => new SongViewModel(o)));
+        }
+
+    }
+}
