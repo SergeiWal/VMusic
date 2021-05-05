03294027f96d02fee4da8e753ff4c316f05baf9e
SergeiWal <korovaabc@gmail.com>
2021-05-05 19:47:05 +0300

    Added initializing db admin default data

---
+++ VMusic/VMusic/Models/VMusicContextInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using VMusic.Hasher;
+
+namespace VMusic.Models
+{
+    class VMusicContextInitializer: DropCreateDatabaseIfModelChanges<VMusicContext>
+    {
+        protected override void Seed(VMusicContext context)
+        {
+            User admin = new User()
+            {
+                Name = "Admin",
+                Login = "Admin",
+                Password = PasswordHasher.GetHash("Admin"),
+                IsAdmin = true,
+                IsBlocked = false
+            };
+
+            context.Users.Add(admin);
+            context.SaveChanges();
+        }
+    }
+}
